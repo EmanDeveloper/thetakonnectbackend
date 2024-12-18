d37caef205b922494778aca53ba75c5c287267ea
EmanDeveloper <memankhadim@gmail.com>
2024-12-18 22:43:58 +0500

    secure

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 3),
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      secure: true, 
+      // secure: true, 
       sameSite: 'strict' ,
     }
    
